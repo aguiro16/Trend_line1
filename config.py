8b317011b5f558d7e290a98d3f17e64846caa5c6
aguiro16 <aguiro16@gmail.com>
2026-05-14 21:53:54 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+MASSIVE_API_KEY   = os.environ.get("MASSIVE_API_KEY", "")
+MASSIVE_BASE_URL  = "https://api.massive.com"
+
+TELEGRAM_TOKEN    = os.environ.get("TELEGRAM_TOKEN", "")
+TELEGRAM_CHAT_ID  = os.environ.get("TELEGRAM_CHAT_ID", "")
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
